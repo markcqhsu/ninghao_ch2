6cccad19c468aedefbf3c501ea7ffb521b279d88
Mark <kkboytest@gmail.com>
2021-06-22 15:32:17 +0800

    TabBar Demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import './Demo/listview-demo.dart';
 
 void main() => runApp(
       App(), //自己定義的Widget
@@ -14,6 +13,8 @@ class App extends StatelessWidget {
       home: Home(),
       theme: ThemeData(
         primarySwatch: Colors.yellow,
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+        splashColor: Colors.white70,//水波紋效果
       ),
     );
   }
@@ -22,27 +23,47 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        //一般都會用在Scaffold裏面
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: "Navigation",
-          onPressed: () => debugPrint("Navigation button is pressed"),
-        ),
-        title: Text("塑蓋"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: "Search",
-            onPressed: () => debugPrint("Search button is pressed"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          //一般都會用在Scaffold裏面
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            tooltip: "Navigation",
+            onPressed: () => debugPrint("Navigation button is pressed"),
           ),
-        ],
-        elevation: 0.0,
+          title: Text("塑蓋"),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.search),
+              tooltip: "Search",
+              onPressed: () => debugPrint("Search button is pressed"),
+            ),
+          ],
+          elevation: 0.0,
+          bottom: const TabBar(
+            unselectedLabelColor: Colors.black38,
+            indicatorColor: Colors.black54,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+        ),
+        // body: ListViewDemo(),
+        body: const TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12,),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12,),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12,),
+          ],
+        ),
       ),
-      // body: ListViewDemo(),
-      body: null,
     );
   }
 }
