b70ba845bca66a893c5ab1d7240e3c12ef6fe884
Mark <kkboytest@gmail.com>
2021-07-01 13:19:00 +0800

    scoped_model 不支援null safety, 所以就先pass了

--- lib/Demo/state/state_management_demo.dart
+++ lib/Demo/state/state_management_demo.dart
@@ -1,41 +1,65 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class StateManagementDemo extends StatefulWidget {
-
-  @override
-  _StateManagementDemoState createState() => _StateManagementDemoState();
-}
-
-class _StateManagementDemoState extends State<StateManagementDemo> {
-  int _count = 0;
-  void _increaseCount(){
-    setState(() {
-      _count+=1;
-      print(_count);
-    });
-  }
-
+//------使用ScopedModel
+class StateManagementDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
-      count: _count,
-      increaseCount: _increaseCount,
+    return ScopedModel(
+      model: CounterModel(),
       child: Scaffold(
         appBar: AppBar(
           title: Text("StateManagementDemo"),
           elevation: 0.0,
         ),
         body: CounterWrapper(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: _increaseCount,
+        floatingActionButton: ScopedModelDescendant<CounterModel>(
+          rebuildOnChange: false,
+          builder: (context, _, model) => FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: model.increaseCount,
+          ),
         ),
       ),
     );
   }
 }
+
+//-----原本的------
+// class StateManagementDemo extends StatefulWidget {
+//
+//   @override
+//   _StateManagementDemoState createState() => _StateManagementDemoState();
+// }
+
+// class _StateManagementDemoState extends State<StateManagementDemo> {
+//   int _count = 0;
+//   void _increaseCount(){
+//     setState(() {
+//       _count+=1;
+//       print(_count);
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return CounterProvider(
+//       count: _count,
+//       increaseCount: _increaseCount,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text("StateManagementDemo"),
+//           elevation: 0.0,
+//         ),
+//         body: CounterWrapper(),
+//         floatingActionButton: FloatingActionButton(
+//           child: Icon(Icons.add),
+//           onPressed: _increaseCount,
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class CounterWrapper extends StatelessWidget {
   // final int count;//從父被接受參數
@@ -50,24 +74,32 @@ class CounterWrapper extends StatelessWidget {
   }
 }
 
-
 class Counter extends StatelessWidget {
   //因為透過inheritted Widget所以都不需要這樣傳遞參數
   // final int count;//從父被接受參數
   // final VoidCallback increaseCount;//從父輩接受回調
   // Counter(this.count, this.increaseCount);
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   final int count = CounterProvider.of(context).count;
+  //   final VoidCallback increaseCount = CounterProvider.of(context).increaseCount;
+  //   return ActionChip(
+  //     label: Text("$count"),//因為是內部參數, 所以加上_
+  //     onPressed:  increaseCount,
+  //   );
+  // }
+
+  //------使用ScopedModel------
   @override
   Widget build(BuildContext context) {
-    final int count = CounterProvider.of(context).count;
-    final VoidCallback increaseCount = CounterProvider.of(context).increaseCount;
-    return ActionChip(
-      label: Text("$count"),//因為是內部參數, 所以加上_
-      onPressed:  increaseCount,
-    );
+    return ScopedModelDescendant<CounterModel>(
+        builder: (context, _, model) => ActionChip(
+              label: Text("${model.count}"),
+              onPressed: model.increaseCount,
+            ));
   }
 }
-
 
 // class StateManagementDemo extends StatelessWidget {
 //   int count = 0;
@@ -100,7 +132,6 @@ class CounterProvider extends InheritedWidget {
   final VoidCallback increaseCount;
   final Widget child;
 
-
   CounterProvider({
     required this.count,
     required this.increaseCount,
@@ -108,7 +139,8 @@ class CounterProvider extends InheritedWidget {
   }) : super(child: child);
 
   static CounterProvider of(BuildContext context) {
-    final CounterProvider? result = context.dependOnInheritedWidgetOfExactType<CounterProvider>();
+    final CounterProvider? result =
+        context.dependOnInheritedWidgetOfExactType<CounterProvider>();
     assert(result != null, 'No CounterProvider found in context');
     return result!;
   }
@@ -116,5 +148,16 @@ class CounterProvider extends InheritedWidget {
   @override
   bool updateShouldNotify(CounterProvider old) {
     return true;
+  }
+}
+
+class CounterModel extends Model {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increaseCount() {
+    _count += 1;
+    notifyListeners();
   }
 }
