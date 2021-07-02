4f14074217794461de9c932342e4971e1a516029
Mark <kkboytest@gmail.com>
2021-07-02 09:01:41 +0800

    Map 2 Json Json 2 Map

--- lib/Demo/http/http_demo.dart
+++ lib/Demo/http/http_demo.dart
@@ -5,7 +5,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class HttpDemo extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,40 +18,48 @@ class HttpDemo extends StatelessWidget {
 }
 
 class HttpDemoHome extends StatefulWidget {
-
   @override
   _HttpDemoHomeState createState() => _HttpDemoHomeState();
 }
 
 class _HttpDemoHomeState extends State<HttpDemoHome> {
-
   @override
   void initState() {
     super.initState();
     // fetchPost();
-    final post = {  //新增一個Post數據, 裡面對應的是Map的值
-      "title":"hello",
-      "description":"nice to meet you",
+    final post = {
+      //新增一個Post數據, 裡面對應的是Map的值
+      "title": "hello",
+      "description": "nice to meet you",
     };
-    print(post["title"]);//map數據輸出方式
+    print(post["title"]); //map數據輸出方式
     print(post["description"]);
 
-    final postJson = json.encode(post);//把map轉換成json
+    final postJson = json.encode(post); //把map轉換成json
     print(postJson);
-
-
   }
 
-  fetchPost() async{
+  fetchPost() async {
     var url = Uri.parse("https://resources.ninghao.net/demo/posts.json");
     final response = await http.get(url);
     print("statusCode: ${response.statusCode}");
     print("body: ${response.body}");
-
-}
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+}
+
+class Post {
+  final String title;
+  final String description;
+
+  Post(this.title,
+      this.description,);
+
+  Post.fromJson(Map json)
+      : title = json["title"],
+        description = json["description"];
 }
