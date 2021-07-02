ff6430acbdaa8c557098fb168b09c8c2958d4d62
Mark <kkboytest@gmail.com>
2021-07-02 08:57:41 +0800

    Map 2 Json Json 2 Map

--- lib/Demo/http/http_demo.dart
+++ lib/Demo/http/http_demo.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HttpDemo extends StatelessWidget {
 
@@ -28,7 +29,18 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
   @override
   void initState() {
     super.initState();
-    fetchPost();
+    // fetchPost();
+    final post = {  //新增一個Post數據, 裡面對應的是Map的值
+      "title":"hello",
+      "description":"nice to meet you",
+    };
+    print(post["title"]);//map數據輸出方式
+    print(post["description"]);
+
+    final postJson = json.encode(post);//把map轉換成json
+    print(postJson);
+
+
   }
 
   fetchPost() async{
