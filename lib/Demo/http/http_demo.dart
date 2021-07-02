bcc0d43fc09685f2a249bc7b544f72c7aaa1f963
Mark <kkboytest@gmail.com>
2021-07-02 09:33:24 +0800

    使用请求数据：把响应的数据转换成自定义 Dart 对象

--- lib/Demo/http/http_demo.dart
+++ lib/Demo/http/http_demo.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class HttpDemo extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,33 +19,59 @@ class HttpDemo extends StatelessWidget {
 }
 
 class HttpDemoHome extends StatefulWidget {
+
   @override
   _HttpDemoHomeState createState() => _HttpDemoHomeState();
 }
 
 class _HttpDemoHomeState extends State<HttpDemoHome> {
+
   @override
   void initState() {
     super.initState();
-    // fetchPost();
-    final post = {
-      //新增一個Post數據, 裡面對應的是Map的值
-      "title": "hello",
-      "description": "nice to meet you",
-    };
-    print(post["title"]); //map數據輸出方式
-    print(post["description"]);
+    fetchPosts()
+    .then((value) => print(value));
 
-    final postJson = json.encode(post); //把map轉換成json
-    print(postJson);
+    // final post = {  //新增一個Post數據, 裡面對應的是Map的值
+    //   "title":"hello",
+    //   "description":"nice to meet you",
+    // };
+    // print(post["title"]);//map數據輸出方式
+    // print(post["description"]);
+    //
+    // final postJson = json.encode(post);//把map轉換成json
+    // print(postJson);
+    //
+    // final postJsonConverted = json.decode(postJson); //把Json轉換為Map
+    // print(postJsonConverted["title"]);
+    // print(postJsonConverted["description"]);
+    // print(postJsonConverted["description"]);
+    // print(postJsonConverted is Map);
+    //
+    // final postModel = Post.fromJson(postJsonConverted);//現在這個postModel 是一個post 類型的數據
+    // print("title: ${postModel.title}, description: ${postModel.description} ");
+    //
+    // print("${json.encode(postModel)}");//輸出一個使用Josn.encode轉換之後的model
   }
 
-  fetchPost() async {
+  Future<List<Post>> fetchPosts() async{
     var url = Uri.parse("https://resources.ninghao.net/demo/posts.json");
     final response = await http.get(url);
-    print("statusCode: ${response.statusCode}");
-    print("body: ${response.body}");
-  }
+    // print("statusCode: ${response.statusCode}");
+    // print("body: ${response.body}");
+    if (response.statusCode ==200){
+      final responseBody = json.decode(response.body);
+      List<Post> posts = responseBody["posts"]
+      .map<Post>((item)=>Post.fromJson(item))//利用map去處理一下列表數據, 返回post, 當前項目叫做item, 把map轉換成post
+      .toList();//把項目轉換成列表
+
+      return posts;
+
+    }else{
+      throw Exception("Failed to fetch posts.");
+    }
+
+}
 
   @override
   Widget build(BuildContext context) {
@@ -53,13 +80,24 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
 }
 
 class Post {
+  final int id;
   final String title;
   final String description;
+  final String author;
+  final String imageUrl;
 
   Post(this.title,
-      this.description,);
+      this.description, this.id, this.author, this.imageUrl,);
 
   Post.fromJson(Map json)
-      : title = json["title"],
-        description = json["description"];
+      : id = json["id"],
+        title = json["title"],
+        description = json["description"],
+        author = json["author"],
+        imageUrl = json["imageUrl"];
+
+  Map toJson()=>{
+    "title":title,
+    "description":description,
+  };
 }
