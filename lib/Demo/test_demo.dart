5e7e70deb14f28e725eae4e7914af467c9471c00
Mark <kkboytest@gmail.com>
2021-07-02 16:51:29 +0800

    Widget testing. command: flutter test test/nighao_demo_test.dart

--- lib/Demo/test_demo.dart
+++ lib/Demo/test_demo.dart
@@ -9,7 +9,37 @@ class TestDemo extends StatelessWidget {
         title: Text("TestDemo"),
         elevation: 0.0,
       ),
+      body: TestDemoHome(),
     );
+  }
+}
+class TestDemoHome extends StatefulWidget {
+
+  @override
+  _TestDemoHomeState createState() => _TestDemoHomeState();
+}
+
+class _TestDemoHomeState extends State<TestDemoHome> {
+
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Chip(
+          label: Text("hello"),
+        ),
+        ActionChip(
+          label: Text("$count"),
+          onPressed: (){
+            setState(() {
+              count++;
+            });
+          },
+        )
+      ],
+    );
+
   }
 }
 
--- test/nighao_demo_test.dart
+++ test/nighao_demo_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_0622_01/Demo/test_demo.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -6,5 +7,28 @@ void main(){
   test("should return hellp + something", (){
     var string = NinghaoTestDemo.greet("ninghao");
     expect(string, "hello ninghao");
+  });
+  
+  
+  testWidgets("widget testing demo", (WidgetTester tester) async{
+    await tester.pumpWidget(
+      MaterialApp(home: TestDemo())
+    );
+    
+    final labelText = find.text("hello");
+    // expect(labelText, findsNothing);
+    // expect(labelText, findsOneWidget);
+    expect(labelText, findsNWidgets(1));
+
+    final actionChipLabelText = find.text("0");
+    expect(actionChipLabelText, findsOneWidget);
+    
+    final actionChip = find.byType(ActionChip);
+    await tester.tap(actionChip);
+    await tester.pump();
+    
+    final actionChipLabelTextAfterTap = find.text("1");
+    expect(actionChipLabelTextAfterTap, findsOneWidget);
+    expect(actionChipLabelText, findsNothing);
   });
 }
